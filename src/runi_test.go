3ec3b5891c759ec0b0a073971b954d3c01d9bd57
Nipun Wahi <nipunwahi@pop-os.localdomain>
2021-03-09 18:32:23 +0530

    Added more tests

--- src/runi_test.go
+++ src/runi_test.go
@@ -42,3 +42,154 @@ func TestSimple(t *testing.T) {
 
 	checkOutput(t, "tests/simple.txt", output[:])
 }
+
+func TestBig(t *testing.T) {
+	output := [...]Token{
+		{keywords["int"], "int", 1},
+		{IDENT, "main", 1},
+		{LPAREN, "(", 1},
+		{RPAREN, ")", 1},
+		{LBRACE, "{", 1},
+		{keywords["int"], "int", 2},
+		{IDENT, "x", 2},
+		{ASSIGN, "=", 2},
+		{INTEGER, "5", 2},
+		{SEMICOLON, ";", 2},
+		{keywords["int"], "int", 3},
+		{IDENT, "j", 3},
+		{ASSIGN, "=", 3},
+		{INTEGER, "0", 3},
+		{SEMICOLON, ";", 3},
+		{keywords["for"], "for", 4},
+		{LPAREN, "(", 4},
+		{keywords["int"], "int", 4},
+		{IDENT, "i", 4},
+		{ASSIGN, "=", 4},
+		{INTEGER, "0", 4},
+		{SEMICOLON, ";", 4},
+		{IDENT, "i", 4},
+		{COMP_OP, "<", 4},
+		{INTEGER, "10", 4},
+		{SEMICOLON, ";", 4},
+		{IDENT, "i", 4},
+		{ASSIGN, "=", 4},
+		{IDENT, "i", 4},
+		{PLUS, "+", 4},
+		{INTEGER, "1", 4},
+		{RPAREN, ")", 4},
+		{LBRACE, "{", 4},
+		{keywords["if"], "if", 5},
+		{LPAREN, "(", 5},
+		{IDENT, "x", 5},
+		{COMP_OP, "<", 5},
+		{IDENT, "i", 5},
+		{RPAREN, ")", 5},
+		{LBRACE, "{", 5},
+		{IDENT, "j", 6},
+		{ASSIGN, "=", 6},
+		{IDENT, "j", 6},
+		{PLUS, "+", 6},
+		{INTEGER, "1", 6},
+		{SEMICOLON, ";", 6},
+		{RBRACE, "}", 7},
+		{RBRACE, "}", 8},
+		{RBRACE, "}", 9},
+	}
+	checkOutput(t, "tests/big.txt", output[:])
+}
+
+func TestCheckLine(t *testing.T) {
+	output := [...]Token{
+		{keywords["int"], "int", 5},
+		{IDENT, "main", 5},
+		{LPAREN, "(", 5},
+		{RPAREN, ")", 5},
+		{LBRACE, "{", 5},
+		{keywords["int"], "int", 8},
+		{IDENT, "x", 8},
+		{ASSIGN, "=", 8},
+		{INTEGER, "5", 9},
+		{SEMICOLON, ";", 9},
+		{keywords["int"], "int", 10},
+		{IDENT, "j", 10},
+		{ASSIGN, "=", 10},
+		{INTEGER, "0", 10},
+		{SEMICOLON, ";", 10},
+		{keywords["for"], "for", 11},
+		{LPAREN, "(", 11},
+		{keywords["int"], "int", 11},
+		{IDENT, "i", 11},
+		{ASSIGN, "=", 11},
+		{INTEGER, "0", 11},
+		{SEMICOLON, ";", 11},
+		{IDENT, "i", 12},
+		{COMP_OP, "<", 12},
+		{INTEGER, "10", 12},
+		{SEMICOLON, ";", 12},
+		{IDENT, "i", 12},
+		{ASSIGN, "=", 12},
+		{IDENT, "i", 12},
+		{PLUS, "+", 12},
+		{INTEGER, "1", 12},
+		{RPAREN, ")", 12},
+		{LBRACE, "{", 12},
+		{keywords["if"], "if", 13},
+		{LPAREN, "(", 13},
+		{IDENT, "x", 13},
+		{COMP_OP, "<", 13},
+		{IDENT, "i", 13},
+		{RPAREN, ")", 13},
+		{LBRACE, "{", 13},
+		{IDENT, "j", 14},
+		{ASSIGN, "=", 14},
+		{IDENT, "j", 14},
+		{PLUS, "+", 14},
+		{INTEGER, "1", 14},
+		{SEMICOLON, ";", 14},
+		{RBRACE, "}", 15},
+		{RBRACE, "}", 16},
+		{RBRACE, "}", 19},
+	}
+	checkOutput(t, "tests/checkLine.txt", output[:])
+}
+
+func TestRandom(t *testing.T) {
+	output := [...]Token{
+		{STRING, "\"hello i am a string\"", 1},
+		{STRING, "\"another one\"", 2},
+		{INTEGER, "1234", 3},
+		{INTEGER, "99192", 4},
+		{PLUS, "+", 5},
+		{ASSIGN, "=", 5},
+		{MINUS, "-", 5},
+		{STAR, "*", 5},
+		{SLASH, "/", 5},
+		{COMP_OP, "!=", 5},
+	}
+
+	checkOutput(t, "tests/random.txt", output[:])
+}
+
+func TestIllegal(t *testing.T) {
+	output := [...]Token{
+		{ILLEGAL, "&", 1},
+		{IDENT, "aa", 1},
+		{ILLEGAL, "!", 2},
+		{INTEGER, "123", 3},
+	}
+
+	checkOutput(t, "tests/illegal.txt", output[:])
+}
+
+func TestComments(t *testing.T) {
+	output := [...]Token{
+		{COMMENT, COMMENT, 1},
+		{keywords["int"], "int", 3},
+		{IDENT, "x", 3},
+		{ASSIGN, "=", 3},
+		{INTEGER, "1", 3},
+		{SEMICOLON, ";", 3},
+	}
+
+	checkOutput(t, "tests/comments.txt", output[:])
+}
